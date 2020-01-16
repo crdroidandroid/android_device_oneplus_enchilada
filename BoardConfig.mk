ba2be6caf5bf20168e1942bb6572e542e343dea7
firebird11 <hbgassel@gmail.com>
2020-01-16 18:01:20 +0100

    enchilada: Switch to kronic defconfig
    Change-Id: Ic02b18b05d835d5fa0687dc32b333c5cccfc4075

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@ DEVICE_PATH := device/oneplus/enchilada
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
 # Kernel
-TARGET_KERNEL_CONFIG := enchilada_defconfig
+TARGET_KERNEL_CONFIG := kronic_defconfig
 
 # Recovery
 TARGET_PREBUILT_RECOVERY_RAMDISK_CPIO := $(DEVICE_PATH)/ramdisk-recovery.cpio
