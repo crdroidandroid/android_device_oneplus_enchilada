1d7159d0e23a91c17eeb8d02f6593ddcf4b00f0b
Terminator-J <terminator_j@hotbutteredmonkey.com>
2024-05-26 12:25:41 -0700

    Revert "enchilada: Ship cgroups.json and task_profiles.json"
    This reverts commit b47cacd8670da44038903acc5f322e3a4b04ebe5.
    
    Reason for revert: We're going to include commonized cgroups & task_profiles in sdm845-common to override ROM-side inclusion of CAF shenanigans.

--- device.mk
+++ device.mk
@@ -58,11 +58,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OnePlusPocketMode
 
-# Power
-PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
